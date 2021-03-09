fee15943858af22412467f68a02487469d3c6a23
Rodrigo Dominguez <rodrigo.dominguez@mercadolibre.com>
2021-03-09 16:21:21 -0300

    Delete RappiTest directory

--- RappiTest/app/src/main/java/com/rodrigodominguez/rappitest/api/FilmsHelper.kt
+++
@@ -1,7 +0,0 @@
-package com.rodrigodominguez.rappitest.api
-
-class FilmsHelper (private val apiService: FilmsService) {
-    suspend fun getFilms(year: String) = apiService.getFilms(year)
-
-    suspend fun getFilm(id: String) = apiService.getFilm(id)
-}
--- RappiTest/app/src/main/java/com/rodrigodominguez/rappitest/filmdetail/data/Film.kt
+++
@@ -1,5 +0,0 @@
-package com.rodrigodominguez.rappitest.filmdetail.data
-
-data class Film(
-    val imdb_id: String,
-    val id: Int)
